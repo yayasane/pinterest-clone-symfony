ff4fdd15b66c1ebb9de4532ddfb333b73cf46288
yayaveli <yabadji2010@gmail.com>
2021-02-07 07:08:54 +0000

    Display pin owner and add getFullname method for user's full name generation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -191,4 +191,9 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function getFullName()
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
 }
